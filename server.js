5e8632751b524341e1560017981402a422f91970
Shannon Ervin <sjervin07@gmail.com>
2020-04-04 19:08:59 -0500

    added dotenv, moved folders, application running

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require('cors');
+
+require('dotenv/config')
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,10 +16,28 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost:27017/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+const db = process.env.MONGODB_URL;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+connectDB()
+
+// cors origin URL - Allow inbound traffic from origin
+corsOptions = {
+  origin: "Your FrontEnd Website URL",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 
 // routes
 app.use(require("./routes/api.js"));
